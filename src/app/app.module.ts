58334e1ff41634e21c36c175770eda7f7c6fc047
Luis Angel Santos <santosangel748@gmail.com>
2023-02-02 12:42:29 -0600

    Configuracion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'prade-f231b',
+    appId: '1:1094780554833:web:68e1aace8aa90745c03270',
+    databaseURL: 'https://prade-f231b-default-rtdb.firebaseio.com',
+    storageBucket: 'prade-f231b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyANycWTeZ_56ZSAmoKNbpCm1oWb-IB7WWQ',
+    authDomain: 'prade-f231b.firebaseapp.com',
+    messagingSenderId: '1094780554833',
+    measurementId: 'G-TTT3SWPVB1',
+  },
+  production: false
+};
